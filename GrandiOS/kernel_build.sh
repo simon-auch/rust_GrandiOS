0136f7d1f40d700f2e6e9767d632871a2ed83d31
Raidwas <raidwas@users.noreply.github.com>
2017-10-23 15:32:37 +0200

    stuff

--- GrandiOS/kernel_build.sh
+++ GrandiOS/kernel_build.sh
@@ -3,6 +3,8 @@
 LINKER_SIMON="arm-linux-gnueabihf-ld"
 LINKER_FU="/home/mi/linnert/arm/bin/arm-none-eabi-ld"
 
+LINKER=$LINKER_FU
+
 #make sure cargo,rustup,xargo is in the path
 source $HOME/.cargo/env
 
@@ -10,4 +12,4 @@ source $HOME/.cargo/env
 xargo clean
 xargo build --target armv4t-none-eabi
 #link + cleanup
-$LINKER_FU --gc-sections -Tkernel.lds -o kernel target/armv4t-none-eabi/debug/libGrandiOS.a
+$LINKER --gc-sections -Tkernel.lds -o kernel target/armv4t-none-eabi/debug/libGrandiOS.a
--- GrandiOS/kernel_dump.sh
+++ GrandiOS/kernel_dump.sh
@@ -3,4 +3,6 @@
 DUMP_SIMON="arm-linux-gnueabihf-objdump"
 DUMP_FU="/home/mi/linnert/arm/bin/arm-none-eabi-objdump"
 
-$DUMP_FU -fhd kernel > kernel.dump
+DUMP=$DUMP_FU
+
+$DUMPU -fhd kernel > kernel.dump
