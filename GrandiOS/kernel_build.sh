df929d8c30ec19c0a9fca42dbf1a28044b160881
myname <myname@ks300980.kimsufi.com>
2017-10-27 15:54:33 +0200

    kernel_build generalized

--- GrandiOS/kernel_build.sh
+++ GrandiOS/kernel_build.sh
@@ -2,11 +2,26 @@
 
 LINKER_SIMON="arm-linux-gnueabihf-ld"
 LINKER_FU="/home/mi/linnert/arm/bin/arm-none-eabi-ld"
+LINKER_ARCH="/usr/arm-none-eabi/bin/ld"
 
 LINKER=$LINKER_FU
+which $LINKER_SIMON >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  LINKER=$LINKER_SIMON
+fi
+which $LINKER_ARCH >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  LINKER=$LINKER_ARCH
+fi
 
 #make sure cargo,rustup,xargo is in the path
-source $HOME/.cargo/env
+if [ -f $HOME/.cargo/env ]; then
+  source $HOME/.cargo/env
+fi
+which xargo >/dev/null 2>&1
+if [ $? -eq 1 ]; then
+  PATH=~/.cargo/bin:$PATH
+fi
 
 #build
 xargo clean
