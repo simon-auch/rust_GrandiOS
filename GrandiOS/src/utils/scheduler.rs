8f2329464befda44d0cec8ca32a0de73de50886d
Raidwas <raidwas@users.noreply.github.com>
2017-11-29 14:00:32 +0100

    improved scheduler

--- GrandiOS/src/utils/scheduler.rs
+++ GrandiOS/src/utils/scheduler.rs
@@ -11,7 +11,7 @@ static mut SCHEDULER: Option<Scheduler> = None;
 
 pub unsafe fn init(current_tcb: TCB){
     SCHEDULER = Some(Scheduler{
-        running: current_tcb,
+        running: Some(current_tcb),
         queue_ready: VecDeque::new(),
         queue_terminate: VecDeque::new(),
         queue_waiting_read: VecDeque::new(),
@@ -37,7 +37,7 @@ pub enum State{
 }
 
 pub struct Scheduler{
-    running: TCB,
+    running: Option<TCB>,
     //Queues for the threads
     queue_ready: VecDeque<TCB>,
     queue_terminate: VecDeque<TCB>,
@@ -47,43 +47,32 @@ pub struct Scheduler{
 }
 
 impl Scheduler{
-    pub fn get_current_tcb(&mut self) -> &mut TCB{
-        &mut self.running
-    }
     pub fn add_thread(&mut self, tcb: TCB){
         self.queue_ready.push_back(tcb);
     }
     pub fn switch(&mut self, register_stack: &mut software_interrupt::RegisterStack, new_state: State){
-        let mut next_tcb = self.queue_ready.pop_front();
-        match next_tcb {
-            None => {
-                //There is no other thread to which we could switch, so we dont switch.
-                //The only moment where this should be possible to happen, is if the idle thread gets interrupted by an interrupt, but no other thread is ready.
-                //This could be avoided if we would have 2 idle threads.
-                //since we dont switch we can just ignore the new_state and the register_stack.
-            },
-            Some(mut next_tcb) => {
-                self.running.save_registers(&register_stack);
-                next_tcb.load_registers(register_stack);
-                let mut old_running = replace(&mut self.running, next_tcb);
-                //make sure the old thread gets the correct state and gets moved into the correct Queue
-                match new_state{
-                    State::Ready       => { &mut self.queue_ready },
-                    State::Terminate   => { &mut self.queue_terminate },
-                    State::WaitingRead => { 
-                        match self.queue_waiting_read_input.pop_front() {
-                            None => { //there is no input available, so we need to wait
-                                &mut self.queue_waiting_read
-                            },
-                            Some(c) => { //input is available, process it and make the thread ready
-                                software_interrupt::work::read(&mut old_running, c);
-                                &mut self.queue_ready
-                            },
-                        }
+        //save registers for current thread
+        let mut running = self.running.take().unwrap();
+        running.save_registers(&register_stack);
+        //make sure the old thread gets the correct state and gets moved into the correct Queue
+        match new_state{
+            State::Ready       => { &mut self.queue_ready },
+            State::Terminate   => { &mut self.queue_terminate },
+            State::WaitingRead => {
+                match self.queue_waiting_read_input.pop_front() {
+                    None => { //there is no input available, so we need to wait
+                        &mut self.queue_waiting_read
                     },
-                }.push_back(old_running);
-            }
-        }
+                    Some(c) => { //input is available, process it and make the thread ready
+                        software_interrupt::work::read(&mut running, c);
+                        &mut self.queue_ready
+                    },
+                }
+            },
+        }.push_back(running);
+        let mut next = self.queue_ready.pop_front().unwrap();  //muss es immer geben, da idle thread
+        next.load_registers(register_stack);
+        self.running = Some(next);
     }
 
     //set of function to push something into the queue_waiting_*_input queues
